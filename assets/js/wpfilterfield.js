d8b60798c5d61a59182dab630831cfb41b7e358d
VictorAssis <contato@victorassis.com.br>
2019-01-27 23:14:34 -0200

    filter working

---
+++ assets/js/wpfilterfield.js
@@ -0,0 +1,83 @@
+(function($) {
+    //init
+    var params = wp.params;
+    if (typeof params.f === "undefined")
+        params.f = {};
+
+    var add_param = function(key, value) {
+        if (typeof params.f[key] === "undefined")
+            params.f[key] = [];
+        params.f[key].push(value);
+        update_results();
+    }
+
+    var remove_param = function(key, value) {
+        if (typeof params.f[key] !== "undefined") {
+            var index = params.f[key].indexOf(value);
+            if (index > -1)
+                params.f[key].splice(index, 1);
+            if (params.f[key].length == 0)
+                delete params.f[key];
+        }
+        update_results();
+    }
+
+    var update_results = function() {
+        show_loading();
+        //Update URL
+        var paramsTmp = $.extend(true, {}, params);
+        for (var key in paramsTmp.f) {
+            // skip loop if the property is from prototype
+            if (!paramsTmp.f.hasOwnProperty(key)) continue;
+        
+            //implode options
+            paramsTmp.f[key] = paramsTmp.f[key].join(",");
+        }
+        var url = "?" + $.param( paramsTmp );
+        window.history.pushState("", "", url);
+
+        //Update results
+        $.get(document.URL,'',function(data){
+            $(".site-content").html($(data).find(".site-content"));
+            hide_loading();
+        },'html');
+    }
+
+    var show_loading = function () {
+        $(".wpfilterfield-loading").addClass('loading');
+    }
+
+    var hide_loading = function () {
+        $(".wpfilterfield-loading").removeClass('loading');
+    }
+
+    // change
+    $(document).ready(function(){
+        //add loading
+        $('body').prepend("<div class='wpfilterfield-loading'></div>");
+        
+        //explode itens
+        for (var key in params.f) {
+            // skip loop if the property is from prototype
+            if (!params.f.hasOwnProperty(key)) continue;
+        
+            //explode options
+            params.f[key] = params.f[key].split(",");
+    
+            //check options
+            for (let i = 0; i < params.f[key].length; i++) {
+                $('.wpfs-filter-field input[type="checkbox"][name="'+key+'[]"][value="'+params.f[key][i]+'"]').attr('checked','checked');
+            }
+        }
+
+        $('.wpfs-filter-field').on('change', 'input[type="checkbox"]', function(){
+            var name = $(this).attr('name').replace('[]','');
+            var value = $(this).attr('value');
+            if ($(this).is(':checked'))
+                add_param(name,value);
+            else
+                remove_param(name,value);
+        });
+	});
+
+})(jQuery);
---
+++ wp-search-filter.php
@@ -0,0 +1,141 @@
+<?php
+/*
+Plugin Name: WP Search Filter
+Plugin URI:   https://victorassis.com.br/
+Description:  Create filters for your content
+Version:      0.0.1
+Author:       Victor Assis
+Author URI:   https://victorassis.com.br/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wp-search-filter
+Domain Path:  /languages
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registra Widgets
+ */
+class FilterFieldWidget extends WP_Widget {
+
+	function __construct() {
+		// Instantiate the parent object
+		parent::__construct( false, 'WPFS: Filter Field' );
+	}
+
+	function widget( $args, $instance ) {
+        echo '<div class="wpfs-filter-field">';
+        if ( ! empty( $instance['title'] ) )
+            echo "<h2>" . $instance['title'] . "</h2>";
+
+        $field = get_field_object($instance['field']);
+
+        switch ($field['type']) {
+            case 'select':
+                $options = $field['choices'];
+                break;
+            case 'post_object':
+                $posts = get_posts([
+                    'post_type' => $field['post_type'],
+                    'numberposts' => -1,
+                    'orderby' => 'title'
+                ]);
+                $options = [];
+                if ($posts)
+                    foreach ($posts as $post)
+                        $options[$post->ID] = $post->post_title;
+                break;
+            default:
+                $options = [];
+                break;
+        }
+
+        foreach ($options as $key => $value) {
+            echo "<label><input type='checkbox' name='{$field['name']}[]' value='$key' /> $value</label>";
+        }
+
+        echo '</div>';
+	}
+
+	function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'wp-search-filter' );
+		$field = ! empty( $instance['field'] ) ? $instance['field'] : esc_html__( 'Field name', 'wp-search-filter' );
+?>
+		<p>
+		    <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'wp-search-filter' ); ?></label> 
+		    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<p>
+		    <label for="<?php echo esc_attr( $this->get_field_id( 'field' ) ); ?>"><?php esc_attr_e( 'Field:', 'wp-search-filter' ); ?></label> 
+		    <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'field' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'field' ) ); ?>" type="text" value="<?php echo esc_attr( $field ); ?>">
+		</p>
+		<?php 
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+		$instance['field'] = ( ! empty( $new_instance['field'] ) ) ? sanitize_text_field( $new_instance['field'] ) : '';
+
+		return $instance;
+	}
+}
+function wpfilterfield_register_widgets() {
+	register_widget( 'FilterFieldWidget' );
+}
+add_action( 'widgets_init', 'wpfilterfield_register_widgets' );
+
+/**
+ * Filter results
+ */
+add_action('pre_get_posts', 'wpfilterfield_pre_get_posts', 10, 1);
+function wpfilterfield_pre_get_posts( $query ) {
+	// bail early if is in admin
+	if( is_admin() ) return;
+	
+	// bail early if not main query
+	// - allows custom code / plugins to continue working
+    if( !$query->is_main_query() ) return;
+    
+    // bail early if isnt right post type
+    if ( !is_post_type_archive( ['cartao'] ) ) return;
+	
+	// get meta query
+    $meta_query = $query->get('meta_query');
+    
+    // loop over filters
+    if (!isset($_GET['f'])) return;
+	foreach( $_GET['f'] as $key => $values ) {
+		// continue if not found in url
+		if( empty($values) ) {
+			continue;
+		}
+		$value = explode(',',$values);		
+		// append meta query
+    	$meta_query[] = array(
+            'key'		=> $key,
+            'value'		=> $value,
+            'compare'	=> 'IN',
+        );
+        
+    }
+	
+	// update meta query
+	$query->set('meta_query', $meta_query);
+}
+
+/**
+ * Enqueue Scripts
+ */
+function wpfilterfield_enqueue_script() {   
+    wp_enqueue_script( 'wpfilterfield_scripts', plugin_dir_url( __FILE__ ) . 'assets/js/wpfilterfield.js', ['jquery'] );
+    $data = [
+        'params' => $_GET
+    ];
+    wp_localize_script( 'wpfilterfield_scripts', 'wp', $data );
+
+    wp_enqueue_style('wpfilterfield_styles', plugin_dir_url( __FILE__ ) . 'assets/css/wpfilterfield.css');
+}
+add_action('wp_enqueue_scripts', 'wpfilterfield_enqueue_script');
